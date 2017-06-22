7bdbdd48249004c1850b24a3c127e3ad29e48c99
Denis Zavershinskiy <zaverden@gmail.com>
2017-06-22 17:46:52 +0700

    tasks controller and swagger

---
+++ TasksManager/Controllers/TasksController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TasksManager.ViewModel;
+using TasksManager.ViewModel.Filters;
+using TasksManager.ViewModel.Requests;
+using TasksManager.ViewModel.Responses;
+
+namespace TasksManager.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class TasksController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(ListResponse<TaskResponse>))]
+        public async Task<IActionResult> GetTasksListAsync(TaskFilter filter, ListOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(TaskResponse))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateTaskAsync([FromBody]CreateTaskRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{taskId}")]
+        [ProducesResponseType(200, Type = typeof(TaskResponse))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetTaskAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{taskId}")]
+        [ProducesResponseType(200, Type = typeof(TaskResponse))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> UpdateTaskAsync(int taskId, [FromBody]UpdateTaskRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{taskId}")]
+        [ProducesResponseType(204)]
+        public async Task<IActionResult> DeleteTaskAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- TasksManager/Startup.cs
+++ TasksManager/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TasksManager
 {
@@ -25,6 +26,12 @@ namespace TasksManager
         {
             // Add framework services.
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +41,15 @@ namespace TasksManager
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
